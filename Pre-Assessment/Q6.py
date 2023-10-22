a9389769263760dcc30a466c011ef4ba8a25ff4b
Chandler Ruiz <92413632+Chandler-M-Ruiz@users.noreply.github.com>
2023-10-22 14:03:17 -0500

    question creation

---
+++ Pre-Assessment/Q6.py
@@ -0,0 +1,9 @@
+# Create a solution that accepts an integer input representing a 9-digit unformatted student identification number. Output the identification number as a string with no spaces.
+# The solution output should be in the format
+# 111-22-3333
+ 
+# Sample Input/Output:
+# If the input is
+# 154175430
+# then the expected output is
+# 154-17-5430
